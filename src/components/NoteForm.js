0b816438cd527f6f9770e739084e65e82ccc7005
Mateus Luiz <mateuspntx@gmail.com>
2020-07-14 19:06:38 -0300

    Some changes

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,44 +1,43 @@
 import React from 'react'
-import { Form, Button, Input } from 'antd'
+import { Form, Button, Input } from 'antd';
 import { createNote } from '../api'
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
 
-const NoteForm = ({ from, notes, setNotes }) => {
-    
-    const { getFieldDecorator, validateFields, resetFields } = form
+const NoteForm = ({ notes, setNotes }) => {
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        validateFields((err, values) => {
-            if (!err && values.note) {
-                createNote(values.note).then(res => {
-                    const newNotesArray = notes.concat([res])
-                    setNotes(newNotesArray)
-                    toast.success('Added successfully')
-                    resetFields()
-                })
-            }
+  //const { validateFields, resetFields } = form;
+  const [form] = Form.useForm()
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    form.validateFields((err, values) => {
+      if (!err && values.note) {
+        createNote(values.note).then(res => {
+          const newNotesArray = notes.concat([res])
+          setNotes(newNotesArray)
+          toast.success('Added Successfully')
+          form.resetFields()
         })
-    }
+      }
+    });
+  }
+  
+  
+  return (
+    <Form style={{marginBottom: '25px'}} layout="horizontal" onSubmit={handleSubmit}>
+      <Form.Item rules={[{ required: true }]}>
+          <Input
+            className="note-input"
+            size="large"
+            placeholder="Add New Note"
+          />
+          <Button>Create</Button>
+      </Form.Item>
 
-    return (
-        <Form style={{marginBottom: '25px'}} layout="horizontal" onSubmit={handleSubmit}>
-            <Form.Item>
-                {getFieldDecorator('note', {
-                    rules: [],
-                })(
-                    <Input
-                        className="note-input"
-                        size="large"
-                        placeholder="Add new note"
-                    />,
-                    <Button>Create</Button>
-                )}
-            </Form.Item>
-        </Form>
-    )
+  </Form>
+  )
 }
 
-const WrappedNoteForm = Form.create({name: 'notes_form'})(NoteForm)
+//const WrappedNoteForm = Form.create({name: 'notes_form'})(NoteForm)
 
-export default WrappedNoteForm
+export default NoteForm;
